63b8d1661e5fa0b1365dae082cccd398e2feccec
Erin Stalzer <erinstalzer@gmail.com>
2019-05-22 15:48:53 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,5 +1,9 @@
 CREATE TABLE bears (
+<<<<<<< HEAD
     id INTEGER PRIMARY KEY,
+=======
+  id INT PRIMARY KEY,
+>>>>>>> 81238fc18ca1dd41758304498571195b095cb0ec
   name TEXT,
   age INT,
   gender TEXT,
